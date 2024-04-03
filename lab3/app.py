513d41d6f6a8e67feca6a8014f5f740ac822d0a8
vikter13 <viktortenaev@gmail.com>
2024-04-03 14:54:34 +0300

    log and register are done

--- lab3/app.py
+++ lab3/app.py
@@ -71,6 +71,12 @@ def login():
 
     return render_template('login.html', form=form)
 
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/dashboard', methods=['GET', 'POST'])
 @login_required
 def dashboard():
