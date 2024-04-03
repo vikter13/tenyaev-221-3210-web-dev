6bd53c8d6b1d85ec95b665fe91a56d6623354854
vikter13 <viktortenaev@gmail.com>
2024-04-03 13:07:21 +0300

    login and register are done

--- lab3/app.py
+++ lab3/app.py
@@ -1,7 +1,7 @@
 ## basic
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 
 ## forms for auth
 from flask_wtf import FlaskForm
@@ -20,10 +20,20 @@ app.config['SECRET_KEY'] = 'vikter'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login' # если пользователь попытается получить доступ к странице без аутентификации, то его перекинет на login
+
+@login_manager.user_loader # запоминает id user'а для сессии
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
 ## classes
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True) 
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
 
@@ -52,15 +62,25 @@ def home():
 def login():
     form = loginForm()
 
-
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
 
     return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = registerForm()
 
-    if form.validate_on_submit():
+    if form.validate_on_submit(): # возвращает True, если форма была отправлена методом POST и прошла все проверки валидации
         hashed_password = bcrypt.generate_password_hash(form.password.data)
         new_user = User(username=form.username.data, password=hashed_password)
         db.session.add(new_user)
